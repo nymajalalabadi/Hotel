d90b759b1620d20343a0278cae8bee8b98d3b7e6
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-07-01 23:30:46 +0330

    Added Notification For Project

---
+++ Hotel-Web/wwwroot/asset/custom/custom.js
@@ -0,0 +1,18 @@
+﻿function fillPageId(id) {
+    $('#pageId').val(id);
+    $('#filter-search').submit();
+}
+
+function ShowMessage(title,text,theme) {
+    window.createNotification({
+        closeOnClick: true,
+        displayCloseButton: false,
+        positionClass: 'nfc-bottom-right',
+        showDuration: 5000,
+        theme: theme !== '' ? theme : 'success',
+    })({
+        title: title !== '' ? title : 'اعلان',
+        message: text
+    });
+}
+
