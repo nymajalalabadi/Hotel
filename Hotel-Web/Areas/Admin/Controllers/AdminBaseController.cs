8a7856af2b37d822093293922ac91484574ca345
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-07-11 18:29:07 +0330

    Added Admin Areas

---
+++ Hotel-Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel_Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBaseController : Controller
+    {
+        public static readonly string SuccessMessage = "SuccessMessage";
+        public static readonly string WarningMessage = "WarningMessage";
+        public static readonly string InfoMessage = "InfoMessage";
+        public static readonly string ErrorMessage = "ErrorMessage";
+    }
+}
---
+++ Hotel-Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel_Web.Areas.Admin.Controllers
+{
+    public class HomeController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
