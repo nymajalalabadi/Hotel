f632cd13cc865bc003507d26b45ee12125805b12
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-07-24 16:25:04 +0330

    Added FilterHotels At Admin

---
+++ Hotel-Web/Areas/Admin/Controllers/HotelController.cs
@@ -0,0 +1,35 @@
+﻿using Hotel_Application.Services.Interface;
+using Hotel_Domain.ViewModels.Hotels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel_Web.Areas.Admin.Controllers
+{
+    public class HotelController : AdminBaseController
+    {
+        #region constractor
+
+        private readonly IHotelService _hotelService;
+
+        public HotelController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        #endregion
+
+        #region Actions
+
+        #region Hotel
+
+        public async Task<IActionResult> FilterHotels(FilterHotelViewModel filterHotel)
+        {
+            var result = await _hotelService.FilterHotel(filterHotel);
+
+            return View(result);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
