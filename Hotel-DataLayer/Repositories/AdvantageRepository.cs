1ab324bf5c62f3326d624a00439cea8603725895
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-07-29 17:21:44 +0330

    Added Adcange Room Repository

--- Hotel-DataLayer/Repositories/AdvantageRepository.cs
+++ Hotel-DataLayer/Repositories/AdvantageRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Hotel_DataLayer.Context;
+using Hotel_Domain.Entities.Advantage;
 using Hotel_Domain.InterFaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,9 +27,49 @@ namespace Hotel_DataLayer.Repositories
 
         #region Advantage
 
+        public async Task<IQueryable<AdvantageRoom>> GetAllAdvantageRooms()
+        {
+            return _context.AdvantageRooms.Where(a => !a.IsDelete).AsQueryable();
+        }
+
+        public async Task<AdvantageRoom?> GetAdvantageRoomById(long id)
+        {
+            return await _context.AdvantageRooms.FirstOrDefaultAsync(c => c.Id.Equals(id));
+        }
+
+        public async Task AddAdvantageRoom(AdvantageRoom advantageRoom)
+        {
+            await _context.AdvantageRooms.AddAsync(advantageRoom);
+        }
+
+        public void UpdateAdvantageRoom(AdvantageRoom advantageRoom)
+        {
+             _context.AdvantageRooms.Update(advantageRoom);
+        }
+
         #endregion
 
         #region Selected Room To Advantage
+
+        public async Task<IQueryable<SelectedRoomToAdvantage>> GetAllSelectedRoomToAdvantage()
+        {
+            return _context.SelectedRoomToAdvantages.Include(a => a.AdvantageRoom).Where(a => !a.IsDelete).AsQueryable();
+        }
+
+        public async Task<SelectedRoomToAdvantage?> GetSelectedRoomToAdvantageById(long id)
+        {
+            return await _context.SelectedRoomToAdvantages.FirstOrDefaultAsync(c => c.Id.Equals(id));
+        }
+
+        public async Task AddSelectedRoomToAdvantage(SelectedRoomToAdvantage selectedRoomToAdvantage)
+        {
+            await _context.SelectedRoomToAdvantages.AddAsync(selectedRoomToAdvantage);
+        }
+
+        public void UpdateSelectedRoomToAdvantage(SelectedRoomToAdvantage selectedRoomToAdvantage)
+        {
+            _context.SelectedRoomToAdvantages.Update(selectedRoomToAdvantage);
+        }
 
         #endregion
 
--- Hotel-Domain/InterFaces/IAdvantageRepository.cs
+++ Hotel-Domain/InterFaces/IAdvantageRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hotel_Domain.Entities.Advantage;
+using Hotel_Domain.Entities.Hotels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,25 @@ namespace Hotel_Domain.InterFaces
 
         #region Advantage
 
+        Task<IQueryable<AdvantageRoom>> GetAllAdvantageRooms();
+
+        Task<AdvantageRoom?> GetAdvantageRoomById(long id);
+
+        Task AddAdvantageRoom(AdvantageRoom advantageRoom);
+
+        void UpdateAdvantageRoom(AdvantageRoom advantageRoom);
+
         #endregion
 
         #region Selected Room To Advantage
+
+        Task<IQueryable<SelectedRoomToAdvantage>> GetAllSelectedRoomToAdvantage();
+
+        Task<SelectedRoomToAdvantage?> GetSelectedRoomToAdvantageById(long id);
+
+        Task AddSelectedRoomToAdvantage(SelectedRoomToAdvantage selectedRoomToAdvantage);
+
+        void UpdateSelectedRoomToAdvantage(SelectedRoomToAdvantage selectedRoomToAdvantage);
 
         #endregion
 
